a34baa2adbaa574d21fd477a0ac3a7aa6ba79b1c
Heather Pike <heatherlaurenpike@gmail.com>
2015-01-23 15:54:16 -0500

    added Swig and created a test page using render instead of send

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
 
 var app = express();
 
 app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
 
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', './views');
+
+swig.setDefaults({ cache: false });
+
 app.get('/', function (req, res) {
-  res.send('hello, beautiful world!')
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 var port = 3000;
